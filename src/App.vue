709303c329e650f6cfdb52f97fbe102c6ce8e01e
sswq2020 <a309108243@qq.com>
2017-09-17 20:36:13 +0800

    add init

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <v_header :seller="seller"></v_header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to="/goods">其他</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    <router-view :seller="seller"></router-view>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import header from 'components/header/header.vue'
+const ERR_OK = 0;
+export default {
+  data () {
+    return {
+      seller: {}
+    }
+  },
+  created () {
+    this.axios.get('/api/seller').then((response) => {
+      let res = response.data;
+      if (res.errno === ERR_OK) {
+          this.seller = res.data;
+      }
+    })
+  },
+  components: {
+    'v_header': header
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+ @import"./common/stylus/mixin.styl"
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px (rgba(7,17,27,0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        font-size: 14px
+        color: rgb(77, 85, 93)
+        &.active
+          color: rgb(240, 20, 20)
+</style>
